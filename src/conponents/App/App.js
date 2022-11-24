e417baa5c517fc1dc7a8170467d3ddf37865b134
Maks Borenkov <borenkovmv@gmail.com>
2022-11-24 20:33:23 +0200

    Посмотрел подсказку и увидел где не доделал віходной div

--- src/conponents/App/App.js
+++ src/conponents/App/App.js
@@ -1,14 +1,45 @@
-import { Statistics } from '../Statistics/Statistics';
-// import { StatisticsList } from '../Statistics/StatisticsList';
+// import { Statistics } from '../Statistics/Statistics';
+import Statistics from '../Statistics/Statistics';
 import data from '../../fail.json';
 
 function App() {
   return (
     <div>
-      <Statistics title="Upload stats" stats={data} />
-      {/* <Statistics stats={data} /> */}
+      <Statistics title="Upload stats" item={data} />
+      {/* <Statistics stats={data} />  */}
     </div>
   );
+
+  // function LiUser(props) {
+  //   const user = props.user;
+
+  //   return (
+  //     <li>
+  //       {user.label} {user.percentage}
+  //     </li>
+  //   );
+  // }
 }
 
 export default App;
+
+/* <div>
+<Statistics title="Upload stats" stats={data} />
+<Statistics stats={data} /> 
+</div> */
+
+//   <ul>
+//     {users.map((user) => (
+//       <LiUser key={user.id} user={user} />
+//     ))}
+//   </ul>
+// );
+
+// function LiUser(props) {
+//   const user = props.user;
+
+//   return (
+//     <li>
+//       {user.label} {user.percentage}
+//     </li>
+//   );
--- src/conponents/Statistics/Statistics.jsx
+++ src/conponents/Statistics/Statistics.jsx
@@ -1,32 +1,22 @@
 import PropTypes from 'prop-types';
 
-import { StatisticsList } from './StatisticsList';
-
-export const Statistics = ({ stats }) => {
+export default function Statistics({ item }) {
   return (
-    <ul>
-      <li key={stats.id}>
-        <StatisticsList label={stats.label} percentage={stats.percentage} />
-      </li>
-    </ul>
+    <section className="Upload stats">
+      <ul className="">
+        {item.map(({ id, label, percentage }) => (
+          <li className={item} key={id}>
+            <span className={label}>{label}</span>
+            <span className={label}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
-};
+}
 
 Statistics.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })
-  ),
+  id: PropTypes.string,
+  label: PropTypes.string,
+  percentage: PropTypes.number,
 };
-{
-  /* <ul>
-      {stats.map((stat) => (
-        <li key={stat.id}>
-          <StatisticsList label={stat.label} percentage={stat.percentage} />
-        </li>
-      ))}
-    </ul> */
-}
